9dd35d0b4bcf1dcfdeb49c0dfe7ff7b2d5a04453
tamirgrzybowo-afk <tamir.grzybowo@gmail.com>
2026-01-09 19:24:18 +0100

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: process.env.PORT || 3000 });
+const server = new WebSocket.Server({ port: process.env.PORT || 5000 });
 
-console.log('🚀 Laboratoria WebRTC Server started on port', process.env.PORT || 3000);
+console.log('🚀 Laboratoria WebRTC Server started on port', process.env.PORT || 5000);
 
 let broadcaster = null;
 const viewers = new Set();
